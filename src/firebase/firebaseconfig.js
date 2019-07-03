2d7bcab1fffc638f1c24e4a4d7a25fab0aa4b44b
Thomas Andersen <thomas.andersen87@gmail.com>
2019-07-03 17:32:57 +0200

    remove firebaseconfig

--- src/firebase/firebaseconfig.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyB535dw9Tp1PpjrQW6mxOmRfz_dG0obRqg",
-  authDomain: "lamapinball.firebaseapp.com",
-  databaseURL: "https://lamapinball.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
-};
-
-export default config;
